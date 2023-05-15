bef9b1595aea22f6cb85de53d9f59ea7fa48ed5a
Commit feito por iNyzi <nycollas.fuchs@escola.pr.gov.br>
2023-05-15 09:10:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App(){
+  return <h1> Programando em REACT JS</h1>
+}
+
+/*
+  function App(){
+    return <h1> Programando em REACT JS</h1>
+  }
+
+  export default App;
+*/
